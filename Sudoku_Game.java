da3bf4699a7d57f39fb0702c711934e9c91a68c3
admin <koushal.priya.770@gmail.com>
2023-07-21 23:55:06 +0530

    Create Sudoku_Game

---
+++ Sudoku_Game.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sudoku_game;
+/**
+ *
+ * @author hp
+ */
+public class Sudoku_Game {
+
+    public static void main(String[] args) {
+        Sudoku_UI sudokuUI = new Sudoku_UI();
+                sudokuUI.setVisible(true);
+     
+
+
+    }
+}
